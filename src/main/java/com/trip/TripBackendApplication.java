f80dddc283826ef88f4d8f33a1808029371f7acc
sdh20282 <=>
2023-05-02 15:32:16 +0900

    Refactor : Component-Scan 명시

--- src/main/java/com/trip/TripBackendApplication.java
+++ src/main/java/com/trip/TripBackendApplication.java
@@ -2,8 +2,10 @@ package com.trip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.trip"})
 public class TripBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TripBackendApplication.class, args);
