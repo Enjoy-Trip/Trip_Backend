2dcb700db4871e5e411fcd113855ca150515f621
sdh20282 <sdh20282@gmail.com>
2023-05-11 17:46:15 +0900

    Feat : 검색 결과 중간 저장

--- src/main/java/com/trip/searchResult/mapper/SearchResultMapper.java
+++ src/main/java/com/trip/searchResult/mapper/SearchResultMapper.java
@@ -1,8 +1,13 @@
 package com.trip.searchResult.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.trip.searchResult.model.SearchResultDto;
 
 @Mapper
 public interface SearchResultMapper {
-
+	List<SearchResultDto> getSearchResultList();
+	int getSearchResultCount(String word);
 }
