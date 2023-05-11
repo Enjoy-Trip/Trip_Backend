8033c87c2b5e51f05d6ebab39bff4beacb5bc12c
sdh20282 <sdh20282@gmail.com>
2023-05-11 16:09:55 +0900

    Feat : 검색 결과 조회, 업데이트, 삭제 REST API 구현

---
+++ src/main/java/com/trip/searchResult/service/SearchResultService.java
@@ -0,0 +1,5 @@
+package com.trip.searchResult.service;
+
+public interface SearchResultService {
+
+}
---
+++ src/main/java/com/trip/searchResult/service/SearchResultServiceImpl.java
@@ -0,0 +1,10 @@
+package com.trip.searchResult.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class SearchResultServiceImpl implements SearchResultService {
+
+}
