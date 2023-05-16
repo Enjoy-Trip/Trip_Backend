19788b96b0bb06ff3363de1eee8a7c5ae2deda46
JeonghwanKim30 <kimjeonghwan123@gmail.com>
2023-05-16 16:31:48 +0900

    Refactor : 뉴스 객체 반환 수정

--- src/main/java/com/trip/news/controller/NewsController.java
+++ src/main/java/com/trip/news/controller/NewsController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.trip.news.model.NewsDto;
 import com.trip.news.service.NewsService;
+import com.trip.response.model.ResponseDto;
 import com.trip.util.ExceptionHandler;
 
 @RestController
@@ -25,10 +26,21 @@ public class NewsController {
 
 	@GetMapping(value = "")
 	public ResponseEntity<?> newsList(){
+		ResponseDto<List<NewsDto>> response = new ResponseDto<List<NewsDto>>();
 		try {
-			return new ResponseEntity<List<NewsDto>>(newsService.newsList(), HttpStatus.OK);
+			
+			List<NewsDto> newsList = newsService.newsList();
+			if(newsList.size()==0) {
+				response.setState("FAIL");
+				response.setMessage("불러 올 뉴스가 없습니다.");
+			}else {
+				response.setState("SUCCESS");
+				response.setMessage("뉴스 불러오기 성공 했습니다.");
+				response.setData(newsList);
+			}
+			return new ResponseEntity<ResponseDto<List<NewsDto>>>(response, HttpStatus.OK);
 		}catch (Exception e) {
-			return ExceptionHandler.exceptionHandling(e);
+			return ExceptionHandler.exceptionResponse(response, e);
 		}
 	}
 }
